1665102a028c33541adf024a88115383deedec40
Carolyn Saunders <gigavinyl@posteo.is>
2018-05-03 19:52:29 -0700

    Added psuf and youtub-rofi and updated wal.sh

--- wal.sh
+++ wal.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-cp ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi
 cp ~/.cache/wal/colors-luakit.lua ~/.config/luakit/theme.lua
 cp ~/.cache/wal/colors-zathura ~/.config/zathura/zathurarc
